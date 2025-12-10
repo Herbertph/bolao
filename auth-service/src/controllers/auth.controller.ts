0a833e0db7f9823d95ced16ee875a7091524ec91
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2025-12-10 13:48:34 -0400

    Auth Working with bearer token

--- auth-service/src/controllers/auth.controller.ts
+++ auth-service/src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { AuthService } from "../services/auth.services.js";
-
+import { User } from "../models/User.js";
 const service = new AuthService();
 
 export class AuthController {
@@ -20,6 +20,20 @@ export class AuthController {
         req.body.password
       );
       return res.json(result);
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message });
+    }
+  }
+
+  async me(req: any, res: any) {
+    try {
+      const user = await User.findById(req.user.id).select("-passwordHash");
+  
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+  
+      return res.json({ user });
     } catch (err: any) {
       return res.status(400).json({ error: err.message });
     }
--- auth-service/src/routes/auth.routes.ts
+++ auth-service/src/routes/auth.routes.ts
@@ -3,11 +3,13 @@ import { AuthController } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validate.middleware.js";
 import { registerSchema } from "../validators/register.schema.js";
 import { loginSchema } from "../validators/login.schema.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 const controller = new AuthController();
 
 router.post("/register", validate(registerSchema), controller.register);
 router.post("/login", validate(loginSchema), controller.login);
+router.get("/me", authMiddleware, controller.me);
 
 export default router;
